49d15362f850484fcca84425733ab00c11dc60d7
nischalshakya787 <nischalshakya787@gmail.com>
2024-03-07 09:49:24 +0545

    Arranged some files

---
+++ src/data/ColorCode.jsx
@@ -0,0 +1,35 @@
+const colorDigit = {
+  "#000000": 0, // black
+  "#A52A2A": 1, // brown
+  "#FF0000": 2, // red
+  "#FFA500": 3, // orange
+  "#FFFF00": 4, // yellow
+  "#008000": 5, // green
+  "#0000FF": 6, // blue
+  "#8A2BE2": 7, // violet
+  "#808080": 8, // gray
+  "#FFFFFF": 9, // white
+};
+const colorMultiplier = {
+  "#000000": Math.pow(10, 0), // black
+  "#A52A2A": Math.pow(10, 1), // brown
+  "#FF0000": Math.pow(10, 2), // red
+  "#FFA500": Math.pow(10, 3), // orange
+  "#FFFF00": Math.pow(10, 4), // yellow
+  "#008000": Math.pow(10, 5), // green
+  "#0000FF": Math.pow(10, 6), // blue
+  "#8A2BE2": Math.pow(10, 7), // violet
+  "#808080": Math.pow(10, 8), // gray
+  "#FFFFFF": Math.pow(10, 9), // white
+};
+const tolerance = {
+  "#A52A2A": "1%", // brown
+  "#FF0000": "2%", // red
+  "#008000": "0.5%", // green
+  "#0000FF": "0.25%", // blue
+  "#8A2BE2": "0.1%", // violet
+  gold: "5%",
+  silver: "10%",
+};
+
+export { tolerance, colorDigit, colorMultiplier };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,43 +2,10 @@ import React, { useContext, useEffect, useState } from "react";
 import InputBox from "@/components/InputBox";
 import { UserContext } from "@/UserContext";
 import background from "@/assets/image/resistor.png";
-
+import { colorDigit, colorMultiplier, tolerance } from "../data/ColorCode";
 const Home = () => {
   const { color } = useContext(UserContext);
   const [value, setValue] = useState(null);
-  const colorDigit = {
-    "#000000": 0, // black
-    "#A52A2A": 1, // brown
-    "#FF0000": 2, // red
-    "#FFA500": 3, // orange
-    "#FFFF00": 4, // yellow
-    "#008000": 5, // green
-    "#0000FF": 6, // blue
-    "#8A2BE2": 7, // violet
-    "#808080": 8, // gray
-    "#FFFFFF": 9, // white
-  };
-  const colorMultiplier = {
-    "#000000": Math.pow(10, 0), // black
-    "#A52A2A": Math.pow(10, 1), // brown
-    "#FF0000": Math.pow(10, 2), // red
-    "#FFA500": Math.pow(10, 3), // orange
-    "#FFFF00": Math.pow(10, 4), // yellow
-    "#008000": Math.pow(10, 5), // green
-    "#0000FF": Math.pow(10, 6), // blue
-    "#8A2BE2": Math.pow(10, 7), // violet
-    "#808080": Math.pow(10, 8), // gray
-    "#FFFFFF": Math.pow(10, 9), // white
-  };
-  const tolerance = {
-    "#A52A2A": "1%", // brown
-    "#FF0000": "2%", // red
-    "#008000": "0.5%", // green
-    "#0000FF": "0.25%", // blue
-    "#8A2BE2": "0.1%", // violet
-    gold: "5%",
-    silver: "10%",
-  };
   useEffect(() => {
     let filteredArray = color.filter((item) => item !== "");
     if (filteredArray.length != 0) {
@@ -111,7 +78,7 @@ const Home = () => {
   }, [color]);
 
   return (
-    <div className="flex flex-col md:flex-row container bg-black h-[100vh] text-white">
+    <div className="flex flex-col md:flex-row container h-[100vh] ">
       <div className="md:w-1/2 p-4">
         {color.map((digit, index) => (
           <InputBox digit={index} key={index} color={color[index]} />
@@ -122,11 +89,15 @@ const Home = () => {
           <span>{value}</span>
         </div>
         <div className="resistor-box">
+          {/* <div className="bg-black h-screen flex justify-center items-center">
+            <img src={background} class="blend-multiply" alt="Your Image" />
+          </div> */}
           <table
             width={300}
             cellPadding={0}
             cellSpacing={0}
             background={background}
+            style={{ mixBlendMode: "multiply" }}
           >
             <tbody>
               <tr>
